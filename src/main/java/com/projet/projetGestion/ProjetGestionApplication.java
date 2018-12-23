53d2414692b2ea053d539548b852e9aa15b311e5
raph59400 <lourdel.raphael@gmail.com>
2018-12-23 23:16:11 +0100

    [Initialisation] Initialisation du projet.

---
+++ src/main/java/com/projet/projetGestion/ProjetGestionApplication.java
@@ -0,0 +1,14 @@
+package com.projet.projetGestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGestionApplication.class, args);
+	}
+
+}
+
